f140842860bcb784adba7e2e2a00f943a8d07985
Sebastian Waisbrot <sw@demeterr.com>
2014-05-25 11:22:33 -0700

    Add web viewer

--- test/redis-test.js
+++ test/redis-test.js
@@ -63,92 +63,102 @@ d('pipe proxying', function() {
         var todo = 2;
         var ready = function() {
             if (--todo === 0) {
-                redis.createClient().multi([
-                    ['zrange', 'requests:request:start', '0', '-1',
-                        'WITHSCORES'],
-                    ['zrange', 'requests:request:end', '0', '-1',
-                        'WITHSCORES'],
-                    ['zrange', 'requests:response:start', '0', '-1',
-                        'WITHSCORES'],
-                    ['zrange', 'requests:response:end', '0', '-1',
-                        'WITHSCORES'],
-                    ['get', 'request:1:request:url'],
-                    ['get', 'request:1:request:start'],
-                    ['get', 'request:1:request:end'],
-                    ['get', 'request:1:request:data'],
-                    ['hgetall', 'request:1:request:headers'],
-                    ['get', 'request:1:response:status'],
-                    ['get', 'request:1:response:start'],
-                    ['get', 'request:1:response:end'],
-                    ['get', 'request:1:response:data'],
-                    ['hgetall', 'request:1:response:headers'],
-                    ['get', 'request:2:request:url'],
-                    ['get', 'request:2:request:start'],
-                    ['get', 'request:2:request:end'],
-                    ['get', 'request:2:request:data'],
-                    ['hgetall', 'request:2:request:headers'],
-                    ['get', 'request:2:response:status'],
-                    ['get', 'request:2:response:start'],
-                    ['get', 'request:2:response:end'],
-                    ['get', 'request:2:response:data'],
-                    ['hgetall', 'request:2:response:headers']
-                ]).exec(function(err, replies) {
-                    var now = Date.now();
-                    [0, 1, 2, 3].forEach(function(k) {
-                        expect(replies[k].length).to.be(4);
-                        expect(replies[k][0]).to.be('1');
-                        expect(replies[k][2]).to.be('2');
-                        expect(replies[k][1]).to.be.within(start, now);
-                        expect(replies[k][3]).to.be.within(start, now);
+                var client = redis.createClient();
+                client.zrange('requests:request:start', '0', '-1',
+                        function(err, ids) {
+                    client.multi([
+                        ['zrange', 'requests:request:start', '0', '-1',
+                            'WITHSCORES'],
+                        ['zrange', 'requests:request:end', '0', '-1',
+                            'WITHSCORES'],
+                        ['zrange', 'requests:response:start', '0', '-1',
+                            'WITHSCORES'],
+                        ['zrange', 'requests:response:end', '0', '-1',
+                            'WITHSCORES'],
+                        ['get', 'request:' + ids[0] + ':request:url'],
+                        ['get', 'request:' + ids[0] + ':request:start'],
+                        ['get', 'request:' + ids[0] + ':request:end'],
+                        ['get', 'request:' + ids[0] + ':request:data'],
+                        ['hgetall', 'request:' + ids[0] +
+                            ':request:headers'],
+                        ['get', 'request:' + ids[0] + ':response:status'],
+                        ['get', 'request:' + ids[0] + ':response:start'],
+                        ['get', 'request:' + ids[0] + ':response:end'],
+                        ['get', 'request:' + ids[0] + ':response:data'],
+                        ['hgetall', 'request:' + ids[0] +
+                            ':response:headers'],
+                        ['get', 'request:' + ids[1] + ':request:url'],
+                        ['get', 'request:' + ids[1] + ':request:start'],
+                        ['get', 'request:' + ids[1] + ':request:end'],
+                        ['get', 'request:' + ids[1] + ':request:data'],
+                        ['hgetall', 'request:' + ids[1] +
+                            ':request:headers'],
+                        ['get', 'request:' + ids[1] + ':response:status'],
+                        ['get', 'request:' + ids[1] + ':response:start'],
+                        ['get', 'request:' + ids[1] + ':response:end'],
+                        ['get', 'request:' + ids[1] + ':response:data'],
+                        ['hgetall', 'request:' + ids[1] +
+                            ':response:headers']
+                    ]).exec(function(err, replies) {
+                        var now = Date.now();
+                        [0, 1, 2, 3].forEach(function(k) {
+                            expect(replies[k].length).to.be(4);
+                            expect(replies[k][1]).to.be.within(start, now);
+                            expect(replies[k][3]).to.be.within(start, now);
+                        });
+                        expect([
+                            'http://127.0.0.1:8082/url1',
+                            'http://127.0.0.1:8082/url2']).to.contain(
+                            replies[4]);
+                        expect([
+                            'http://127.0.0.1:8082/url1',
+                            'http://127.0.0.1:8082/url2']).to.contain(
+                            replies[14]);
+                        expect(replies[4]).not.to.be(replies[14]);
+
+                        expect(replies[5]).to.be.within(start, now);
+                        expect(replies[6]).to.be.within(start, now);
+                        expect(replies[5]).not.to.be.above(replies[6]);
+
+                        expect(['asd', 'qwe']).to.contain(replies[7]);
+                        expect(['asd', 'qwe']).to.contain(replies[17]);
+                        expect(replies[7]).not.to.be(replies[17]);
+
+                        expect(replies[8].host).to.be('127.0.0.1:8081');
+                        expect(replies[18].host).to.be('127.0.0.1:8081');
+
+                        expect(replies[9]).to.be('200');
+                        expect(replies[19]).to.be('200');
+
+                        expect(replies[10]).to.be.within(start, now);
+                        expect(replies[11]).to.be.within(start, now);
+                        expect(replies[10]).not.to.be.above(replies[11]);
+
+                        expect([
+                            'hello world 1',
+                            'hello world 2']).to.contain(replies[12]);
+                        expect([
+                            'hello world 1',
+                            'hello world 2']).to.contain(replies[22]);
+                        expect(replies[12]).not.to.be(replies[22]);
+
+                        expect(['yes1', 'yes2']).to.contain(
+                                replies[13]['x-test']);
+                        expect(['yes1', 'yes2']).to.contain(
+                                replies[23]['x-test']);
+                        expect(replies[13]['x-test']).not.to.be(
+                                replies[23]['x-test']);
+
+                        expect(replies[15]).to.be.within(start, now);
+                        expect(replies[16]).to.be.within(start, now);
+                        expect(replies[15]).not.to.be.above(replies[16]);
+
+                        expect(replies[20]).to.be.within(start, now);
+                        expect(replies[21]).to.be.within(start, now);
+                        expect(replies[20]).not.to.be.above(replies[21]);
+
+                        done();
                     });
-                    expect([
-                        'http://127.0.0.1:8082/url1',
-                        'http://127.0.0.1:8082/url2']).to.contain(replies[4]);
-                    expect([
-                        'http://127.0.0.1:8082/url1',
-                        'http://127.0.0.1:8082/url2']).to.contain(replies[14]);
-                    expect(replies[4]).not.to.be(replies[14]);
-
-                    expect(replies[5]).to.be.within(start, now);
-                    expect(replies[6]).to.be.within(start, now);
-                    expect(replies[5]).not.to.be.above(replies[6]);
-
-                    expect(['asd', 'qwe']).to.contain(replies[7]);
-                    expect(['asd', 'qwe']).to.contain(replies[17]);
-                    expect(replies[7]).not.to.be(replies[17]);
-
-                    expect(replies[8].host).to.be('127.0.0.1:8081');
-                    expect(replies[18].host).to.be('127.0.0.1:8081');
-
-                    expect(replies[9]).to.be('200');
-                    expect(replies[19]).to.be('200');
-
-                    expect(replies[10]).to.be.within(start, now);
-                    expect(replies[11]).to.be.within(start, now);
-                    expect(replies[10]).not.to.be.above(replies[11]);
-
-                    expect([
-                        'hello world 1',
-                        'hello world 2']).to.contain(replies[12]);
-                    expect([
-                        'hello world 1',
-                        'hello world 2']).to.contain(replies[22]);
-                    expect(replies[12]).not.to.be(replies[22]);
-
-                    expect(['yes1', 'yes2']).to.contain(replies[13]['x-test']);
-                    expect(['yes1', 'yes2']).to.contain(replies[23]['x-test']);
-                    expect(replies[13]['x-test']).not.to.be(
-                            replies[23]['x-test']);
-
-                    expect(replies[15]).to.be.within(start, now);
-                    expect(replies[16]).to.be.within(start, now);
-                    expect(replies[15]).not.to.be.above(replies[16]);
-
-                    expect(replies[20]).to.be.within(start, now);
-                    expect(replies[21]).to.be.within(start, now);
-                    expect(replies[20]).not.to.be.above(replies[21]);
-
-                    done();
                 });
             }
         };
